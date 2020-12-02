e2770b1ec6f2fca745448856ebeac4b94d82ce7f
EmmaXi <xxmhzau@gmail.com>
2020-12-01 21:56:02 -0800

    Updates

---
+++ precache-manifest.454ef56e30119e4c4aaeb11749c24f03.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfb272d669722d7ba7e6a85d841593d",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "46130843546bbd54d47c",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.768f4ad8.chunk.css"
+  },
+  {
+    "revision": "0f61fced7b42e8e35f0e",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.6649a29a.chunk.js"
+  },
+  {
+    "revision": "46130843546bbd54d47c",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.aa5774fd.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "d864426f2b8efb23c063b276c081660c",
+    "url": "/seainfo6150-final-project-webapp/static/media/Kara.d864426f.png"
+  },
+  {
+    "revision": "a876715f266d8f199baa13d615508be1",
+    "url": "/seainfo6150-final-project-webapp/static/media/Mirela.a876715f.jpg"
+  },
+  {
+    "revision": "10e090acafdd255a989afda092f281ef",
+    "url": "/seainfo6150-final-project-webapp/static/media/Sabrina.10e090ac.jpg"
+  },
+  {
+    "revision": "befa7094ee318a1910a3f41ee51cf7a6",
+    "url": "/seainfo6150-final-project-webapp/static/media/introPicture.befa7094.png"
+  },
+  {
+    "revision": "96cb3bc595a85a2f3fbda0e774d97384",
+    "url": "/seainfo6150-final-project-webapp/static/media/login.96cb3bc5.png"
+  }
+]);
